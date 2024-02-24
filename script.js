7571e599dd76ded2a5bef6fd1de3f4c073fe9493
ItaloSavio <italosavioed1972@gmail.com>
2024-02-24 12:12:24 -0300

    subindo pro git

---
+++ script.js
@@ -0,0 +1,14 @@
+function abrirmodal(){
+    let modal = document.querySelector('.modalcorpo');
+    modal.style.display = 'block';
+}
+
+function fechar(){
+    let modal = document.querySelector('.modalcorpo');
+    modal.style.display = 'none';
+}
+
+function fechar1(){
+    let modal = document.querySelector('.modalcorpo');
+    modal.style.display = 'none';
+}
